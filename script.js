01fce59f60115657ab7a944c79525a33296cd478
Joshua Miller <66288776+joshualive@users.noreply.github.com>
2020-09-24 18:58:16 -0400

    Added Gina's image changing javascript

--- script.js
+++ script.js
@@ -63,4 +63,34 @@ function SearchPhotos() {
           document.getElementById("card").innerHTML+=result;
         });
       });
-  }
+}
+
+let i = 1;
+​
+function imageClick() {
+if (!this.alreadyClicked) {
+    addNextImage();
+    counter();
+    this.alreadyClicked = true;
+}
+console.log("imageClick");
+}
+​
+function addNextImage() {
+    let img = `<img id="img-${i}"src=http://placehold.it/${
+    200 + i
+    }> <img id="img2-${i}"src=http://placehold.it/${200 + i}>`;
+    ​
+    document.body.innerHTML = img;
+    ​
+    document.getElementById(`img-${i}`).addEventListener("click", addNextImage);
+    document.getElementById(`img2-${i}`).addEventListener("click", addNextImage);
+    ​
+    i = i + 1;
+}
+
+let firstImage = document.getElementById("firstImage");
+firstImage.addEventListener("click", addNextImage);
+​
+let secondImage = document.getElementById("secondImage");
+secondImage.addEventListener("click", addNextImage);
