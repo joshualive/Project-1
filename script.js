488b896c6cfb78e85007aaf21b3e8a406295a9f9
Joshua Miller <66288776+joshualive@users.noreply.github.com>
2020-09-24 18:34:21 -0400

    Added API functionality into JS script

--- script.js
+++ script.js
@@ -44,3 +44,23 @@ function locationSelector(){
         alert(cityResult)
     }
 }
+
+function SearchPhotos() {
+    let query = document.getElementById("search").value;
+    let url = `https://pixabay.com/api/?key=18404298-c06d772fc6ab5b9c57d22dda6&q=${query}`;
+    //make a request to api
+    fetch(url)
+      .then(function (data) {
+        return data.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        data.hits.forEach((photo) => {
+          //let img = $("<img>");
+          let result = `
+          <img src="${photo.largeImageURL}">
+          `;
+          document.getElementById("card").innerHTML+=result;
+        });
+      });
+  }
