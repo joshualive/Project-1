c2d5da008790c6cce8b41f6484db8755716a8d67
Joshua Miller <66288776+joshualive@users.noreply.github.com>
2020-09-24 22:15:58 -0400

    Added functionality to direct to results page once the progress bar reaches 100%

--- script.js
+++ script.js
@@ -3,18 +3,31 @@ let userSelections = 0
 
 let a = 1
 let b = 1
+let c = 0
 
 function choiceOne() {
     userSelections++
     let imageA = document.getElementById('imageA')
     imageA.innerHTML = `<img src="a${++a}.png" class="choices" onclick="choiceOne()" id="choiceOne" />`
     imageB.innerHTML = `<img src="b${++b}.png" class="choices" onclick="choiceTwo()" id="choiceTwo" />`
+    c += 20
+    document.getElementById('progressBar').value = `${c}`
+    resultsPageDirect()
 }
 
 function choiceTwo() {
     userSelections += 2
     let imageB = document.getElementById('imageB')
+    
     imageB.innerHTML = `<img src="b${++b}.png" class="choices" onclick="choiceTwo()" id="choiceTwo" />`
     imageA.innerHTML = `<img src="a${++a}.png" class="choices" onclick="choiceOne()" id="choiceOne" />`
+    c += 20
+    document.getElementById('progressBar').value = `${c}`
+    resultsPageDirect()
 }
 
+function resultsPageDirect() {
+    if (c === 100) {
+        location.replace("/results.html")
+    }
+}
