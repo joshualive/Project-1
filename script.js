0aaf823d95cb263261497626f072f81e9a0b1056
Joshua Miller <66288776+joshualive@users.noreply.github.com>
2020-09-24 21:31:27 -0400

    Added images and js for rotating through images

--- script.js
+++ script.js
@@ -1,47 +1,20 @@
 // User selections
 let userSelections = 0
 
+let a = 1
+let b = 1
+
 function choiceOne() {
     userSelections++
+    let imageA = document.getElementById('imageA')
+    imageA.innerHTML = `<img src="a${++a}.png" class="choices" onclick="choiceOne()" id="choiceOne" />`
+    imageB.innerHTML = `<img src="b${++b}.png" class="choices" onclick="choiceTwo()" id="choiceTwo" />`
 }
 
 function choiceTwo() {
     userSelections += 2
+    let imageB = document.getElementById('imageB')
+    imageB.innerHTML = `<img src="b${++b}.png" class="choices" onclick="choiceTwo()" id="choiceTwo" />`
+    imageA.innerHTML = `<img src="a${++a}.png" class="choices" onclick="choiceOne()" id="choiceOne" />`
 }
 
-// Image Change
-let i = 1;
-​
-function imageClick() {
-if (!this.alreadyClicked) {
-    addNextImage();
-    counter();
-    this.alreadyClicked = true;
-}
-console.log("imageClick");
-}
-​
-function addNextImage() {
-    let img = `<img id="img-${i}"src=http://placehold.it/${
-    200 + i
-    }> <img id="img2-${i}"src=http://placehold.it/${200 + i}>`;
-    ​
-    document.body.innerHTML = img;
-    ​
-    document.getElementById(`img-${i}`).addEventListener("click", addNextImage);
-    document.getElementById(`img2-${i}`).addEventListener("click", addNextImage);
-    ​
-    i = i + 1;
-}
-
-//Image Listeners
-let firstImage = document.getElementById("firstImage");
-firstImage.addEventListener("click", addNextImage);
-​
-let secondImage = document.getElementById("secondImage");
-secondImage.addEventListener("click", addNextImage);
-
-function clickedImageHandler() {
-    addNextImage()
-    choiceOne()
-}
