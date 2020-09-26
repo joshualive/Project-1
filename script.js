e4655713abfc62df8cb9a71bc2a7e8fb19b40021
Joshua Miller <66288776+joshualive@users.noreply.github.com>
2020-09-26 12:03:50 -0400

    Added in questions array for first & second options. Added if statement to choiceOne & Two to stop updating photos if reaching the end of the quiz.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // User Selections Score
 let userSelections = 0
 
+// User Choices
 let a = 1
 let b = 1
 let c = 0
 
-// User Choices
 function choiceOne() {
     userSelections++
-    let imageA = document.getElementById('imageA')
-    imageA.innerHTML = `<img src="a${++a}.png" class="choices" onclick="choiceOne()" id="choiceOne" />`
-    imageB.innerHTML = `<img src="b${++b}.png" class="choices" onclick="choiceTwo()" id="choiceTwo" />`
+    const imageA = document.getElementById('imageA')
+    const imageB = document.getElementById('imageB')
+    if (a < 5) {
+        imageA.innerHTML = `<img src="a${++a}.png" class="choices" onclick="choiceOne()" id="choiceOne" />`
+        imageB.innerHTML = `<img src="b${++b}.png" class="choices" onclick="choiceTwo()" id="choiceTwo" />`
+    }
     c += 20
     document.getElementById('progressBar').value = `${c}`
     resultsPageDirect()
@@ -18,10 +21,12 @@ function choiceOne() {
 
 function choiceTwo() {
     userSelections += 2
-    let imageB = document.getElementById('imageB')
-    
-    imageB.innerHTML = `<img src="b${++b}.png" class="choices" onclick="choiceTwo()" id="choiceTwo" />`
-    imageA.innerHTML = `<img src="a${++a}.png" class="choices" onclick="choiceOne()" id="choiceOne" />`
+    const imageA = document.getElementById('imageA')
+    const imageB = document.getElementById('imageB')
+    if (a < 5) {
+        imageB.innerHTML = `<img src="b${++b}.png" class="choices" onclick="choiceTwo()" id="choiceTwo" />`
+        imageA.innerHTML = `<img src="a${++a}.png" class="choices" onclick="choiceOne()" id="choiceOne" />`
+    }
     c += 20
     document.getElementById('progressBar').value = `${c}`
     resultsPageDirect()
@@ -36,12 +41,34 @@ function resultsPageDirect() {
 }
 
 //Start Quiz
-let startButton = document.getElementById('startButton')
-let quizQuestions = document.getElementById('quizQuestions')
+const startButton = document.getElementById('startButton')
+const quizQuestions = document.getElementById('quizQuestions')
 
 startButton.addEventListener('click', startQuiz)
 
 function startQuiz() {
     startButton.classList.add('is-hidden')
     quizQuestions.classList.remove('is-hidden')
+    optionOne.textContent = firstOption[0]
+    optionTwo.textContent = secondOption[0]
 }
+
+//Quiz Questions
+const optionOne = document.getElementById('optionOne')
+const optionTwo = document.getElementById('optionTwo')
+
+const firstOption = [  
+     'A hike in the mountains',
+     'Visiting a museum',
+     'Lounging on the beach',
+     'Exploring on your own',
+     'Checking out a non-touristy area',
+]
+
+const secondOption = [
+     'Exploring a city',
+     'Trying new foods',
+     'Shopping for new clothes',
+     'Getting the perfect Instagram photo',
+     'A night at the hottest clubs',
+]
